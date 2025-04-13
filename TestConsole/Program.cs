273a5d994d875f4195be234b64ce22c295d07656
Daniele Del Toso <daniele.deltoso@gmail.com>
2025-04-13 13:46:29 +0200

    Aggiungere i file di progetto.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using FastMediator;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddCustomMediator();
+
+
+
+var provider = services.BuildServiceProvider();
+var mediator = provider.GetRequiredService<Dispatcher>();
+
+try
+{
+    var result = mediator.Send(new Ping ("Hello Mediator!" ));
+    Console.WriteLine($"Risultato: {result}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"[ERROR] {ex.Message}");
+}
+
+mediator.Publish(new SomethingHappened { Message = "Boom" });
+
+
+// ----- Classes di esempio -----
+public class Ping(string message) : IRequest<string>
+{
+    public string Message { get;  } = message;
+}
+
+public class PingHandler : IRequestHandler<Ping, string>
+{
+    public string Handle(Ping request) => $"Risposta a: {request.Message}";
+}
+
+public class SomethingHappened : INotification
+{
+    public string Message { get; set; } = "";
+}
+
+public class SomethingHappenedHandler : INotificationHandler<SomethingHappened>
+{
+    public void Handle(SomethingHappened notification)
+    {
+        Console.WriteLine($"Evento gestito: {notification.Message}");
+    }
+}
---
+++ TestConsole/ValidationBehavior.cs
@@ -0,0 +1,26 @@
+﻿using FastMediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    {
+        public TResponse Handle(TRequest request, Func<TRequest, TResponse> next)
+        {
+            Console.WriteLine($"[Validation] Validating {typeof(TRequest).Name}");
+
+            // Simula errore se il messaggio è vuoto
+            if (request is Ping ping && string.IsNullOrWhiteSpace(ping.Message))
+            {
+                throw new ArgumentException("Ping.Message non può essere vuoto!");
+            }
+
+            return next(request);
+        }
+    }
+}
